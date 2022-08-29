59b4100257bcc7b64f64005a86d110d8acded884
Jaskaran Singh <thecodesingh@gmail.com>
2022-08-29 00:50:28 -0700

    Create handle functions, add tooltip

--- components/CreateSalesOrder.jsx
+++ components/CreateSalesOrder.jsx
@@ -10,6 +10,7 @@ import {
   Td,
   Th,
   Thead,
+  Tooltip,
   Tr
 } from '@chakra-ui/react';
 import { useState } from 'react';
@@ -19,7 +20,7 @@ const CreateSalesOrder = () => {
   const [items, setItems] = useState([
     {
       id: 1,
-      sku: 25551645,
+      sku: '25551645',
       name: 'Carson Dining Set',
       category: 'Dining',
       price: 999,
@@ -29,7 +30,7 @@ const CreateSalesOrder = () => {
     },
     {
       id: 2,
-      sku: 25551035,
+      sku: '25551035',
       name: 'Boyd Dining Set',
       category: 'Dining',
       price: 899,
@@ -38,6 +39,21 @@ const CreateSalesOrder = () => {
         'https://cdn.shopify.com/s/files/1/2660/5106/products/z4vgldhyidbro8mz8tac_1400x.jpg?v=1569295613'
     }
   ]);
+  const [currentSku, setCurrentSku] = useState();
+
+  const handleChange = e => {
+    setCurrentSku(e.target.value);
+  };
+
+  const handleClick = e => {
+    e.preventDefault();
+
+    for (let i = 0; i < items.length; i++) {
+      if (items[i].sku === currentSku) {
+        console.log('Matched...');
+      }
+    }
+  };
 
   return (
     <Box className="sales-order-create">
@@ -47,12 +63,14 @@ const CreateSalesOrder = () => {
         <form>
           <Box className="sku-input">
             <FormControl isRequired>
-              <Input placeholder="Add item by SKU" />
+              <Input placeholder="Add item by SKU" onChange={handleChange} />
             </FormControl>
           </Box>
           <Box className="sku-btn">
             <FormControl>
-              <Button type="submit">Add</Button>
+              <Button type="submit" onClick={handleClick}>
+                Add
+              </Button>
             </FormControl>
           </Box>
         </form>
@@ -92,8 +110,16 @@ const CreateSalesOrder = () => {
                   <Td>${item.price}</Td>
                   <Td>
                     <Box className="sales-item-actions">
-                      <MdDelete size={25} />
-                      <MdInventory2 size={25} />
+                      <Tooltip label="Remove Item">
+                        <span>
+                          <MdDelete size={25} />
+                        </span>
+                      </Tooltip>
+                      <Tooltip label="Check inventory">
+                        <span>
+                          <MdInventory2 size={25} />
+                        </span>
+                      </Tooltip>
                     </Box>
                   </Td>
                 </Tr>
@@ -104,6 +130,7 @@ const CreateSalesOrder = () => {
                 <Td />
                 <Td>Subtotal</Td>
                 <Td>$1898</Td>
+                <Td />
               </Tr>
             </Tbody>
           </Table>
